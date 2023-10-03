bedd748637b70daedce99f6cbf0f16367afa25da
Maksym Sakovych <maximsakovich@gmail.com>
2023-10-03 12:34:30 +0200

    Employee

--- src/homeWork20/Employee.java
+++ src/homeWork20/Employee.java
@@ -2,43 +2,48 @@ package homeWork20;
 
 public class Employee {
 
-        // Приватные поля класса
-        private String name;
-        private int age;
-        private double salary;
+    // Приватные поля класса
+    private String name;
+    private int age;
+    private double salary;
 
-        // Конструктор для инициализации объекта Employee
-        public Employee(String name, int age, double salary) {
-            this.name = name;
-            this.age = age;
-            this.salary = salary;
-        }
+    // Конструктор для инициализации объекта Employee
+    public Employee(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
 
-        // Геттер для имени
-        public String getName() {
-            return name;
-        }
+    // Геттер для имени
+    public String getName() {
+        return name;
+    }
 
-        // Геттер для возраста
-        public int getAge() {
-            return age;
-        }
+    // Геттер для возраста
+    public int getAge() {
+        return age;
+    }
 
-        // Геттер для зарплаты
-        public double getSalary() {
-            return salary;
-        }
+    // Геттер для зарплаты
+    public double getSalary() {
+        return salary;
+    }
 
-        // Сеттер для зарплаты
-        public void setSalary(double salary) {
-            this.salary = salary;
-        }
+    // Сеттер для имени
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        // Метод info() для вывода информации о работнике
-        public void info() {
-            System.out.println("Имя: " + name);
-            System.out.println("Возраст: " + age + " лет");
-            System.out.println("Зарплата: " + salary + " рублей");
-        }
+    // Сеттер для зарплаты
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
-        }
+    // Метод info() для вывода информации о работнике
+    public void info() {
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age + " лет");
+        System.out.println("Зарплата: " + salary + " рублей");
+    }
+
+}
