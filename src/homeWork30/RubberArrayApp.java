1abede32211b880aeb0ef27ef4b570627319d423
Maksym Sakovych <maximsakovich@gmail.com>
2023-10-18 17:09:59 +0200

    Generics

---
+++ src/homeWork30/RubberArrayApp.java
@@ -0,0 +1,22 @@
+package homeWork30;
+
+public class RubberArrayApp {
+
+    public static void main(String[] args) {
+        RubberArray<Integer> ra = new RubberArray<>();
+        ra.add(10, -5, 16, 57);
+        System.out.println(ra);
+        ra.remove(16);
+        System.out.println(ra);
+
+        RubberArray<Integer> raInt = new RubberArray<>();
+        raInt.add(101, 234, 536, -10, 0, 13);
+        System.out.println(raInt.toString());
+
+        RubberArray<String> raStr = new RubberArray<>();
+        raStr.add("Очень", "сложно", "!");
+        System.out.println(raStr.toString());
+
+    }
+}
+
--- src/lesson_30/rubberArray/RubberArrayApp.java
+++ src/lesson_30/rubberArray/RubberArrayApp.java
@@ -2,22 +2,14 @@ package lesson_30.rubberArray;
 
 public class RubberArrayApp {
 
-    public static void main(String[] args) {
-        RubberArray<Integer> ra = new RubberArray<>();
-        ra.add(10, -5, 16, 57);
-        System.out.println(ra);
-        ra.remove(16);
-        System.out.println(ra);
+        public static void main(String[] args) {
+            RubberArray ra = new RubberArray();
+            ra.add(10, -5, 16, 57);
+            System.out.println(ra);
+            ra.remove(16);
+            System.out.println(ra);
 
-        RubberArray<Integer> raInt = new RubberArray<>();
-        raInt.add(101, 234, 536, -10, 0, 13);
-        System.out.println(raInt.toString());
-
-        RubberArray<String> raStr = new RubberArray<>();
-        raStr.add("Очень", "сложно", "!");
-        System.out.println(raStr.toString());
-
-
+//        RubberArray<Integer> raInt = new RubberArray<>();
+//        RubberArray<String> raStr = new RubberArray<>();
+        }
     }
-}
-
