c549551a5b72827f7a56449ca65bd5614b025115
Maksym Sakovych <maximsakovich@gmail.com>
2023-10-21 23:56:14 +0200

    TV

--- src/homeWork33/Backpack/Backpack.java
+++ src/homeWork33/Backpack/Backpack.java
@@ -2,58 +2,80 @@ package homeWork33.Backpack;
 
 public class Backpack {
     public static void main(String[] args) {
-        int[] weights = {3, 4, 5, 8, 9};
-        int[] prices = {1, 6, 4, 7, 6};
+        long startTime = System.currentTimeMillis();
+                int[] weights = {3, 4, 5, 8, 9};
+                int[] prices = {1, 6, 4, 7, 6};
 
-        int maxWeight = 13;
+                int maxWeight = 13;
 
-        long count = 2L << weights.length;
+                long count = 2L << weights.length;
 
-        int maxPrice = 0;
-        long maxState = 0;
+                int maxPrice = 0;
+                long maxState = 0;
+                int maxStateWeight = 0;
+                int itemCount = 0;
+                int totalPrice = 0;
 
-        for (long state = 0; state < count; state++) {
-            int price = statePrice(state, prices);
-            int weight = stateWeight(state, weights);
-            if (weight <= maxWeight) {
-                if (maxPrice < price) {
-                    maxPrice = price;
-                    maxState = state;
+                for (long state = 0; state < count; state++) {
+                    int price = statePrice(state, prices);
+                    int weight = stateWeight(state, weights);
+                    if (weight <= maxWeight) {
+                        if (maxPrice < price) {
+                            maxPrice = price;
+                            maxState = state;
+                            maxStateWeight = weight;
+                            itemCount = 0;
+                            totalPrice = 0;
+                            for (int i = 0; i < weights.length; i++) {
+                                if ((1L << i & maxState) > 0) {
+                                    itemCount++;
+                                    totalPrice += prices[i];
+                                }
+                            }
+                        }
+                    }
                 }
-            }
-        }
 
-        System.out.println("Оптимальное содержимое рюкзака:");
-        long poverOfTwo = 1;
-        for (int i = 0; i < weights.length; i++) {
-            if ((poverOfTwo & maxState) > 0) {
-                System.out.println(i + 1);
-            }
-            poverOfTwo <<= 1;
-        }
-    }
+                System.out.println("Оптимальное содержимое рюкзака:");
+                long powerOfTwo = 1;
+                for (int i = 0; i < weights.length; i++) {
+                    if ((powerOfTwo & maxState) > 0) {
+                        System.out.println("Элемент " + (i + 1) + " - стоимость: " + prices[i] + ", вес: " + weights[i]);
+                    }
+                    powerOfTwo <<= 1;
+                }
 
-    private static int stateWeight(long state, int[] weights) {
-        long poverOfTwo = 1;
-        int weight = 0;
-        for (int i = 0; i < weights.length; i++) {
-            if ((poverOfTwo & state) != 0) {
-                weight += weights[i];
-            }
-            poverOfTwo <<= 1;
-        }
-        return weight;
-    }
+                System.out.println("Число элементов в рюкзаке: " + itemCount);
+                System.out.println("Общая стоимость элементов в рюкзаке: " + totalPrice);
+                System.out.println("Общая стоимость рюкзака: " + maxPrice);
+                System.out.println("Общий вес рюкзака: " + maxStateWeight);
 
-    private static int statePrice(long state, int[] prices) {
-        long poverOfTwo = 1;
-        int price = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if ((poverOfTwo & state) != 0) {
-                price += prices[i];
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Время выполнения: " + duration + " миллисекунд.");
             }
-            poverOfTwo <<= 1;
-        }
-        return price;
+
+            private static int stateWeight(long state, int[] weights) {
+                long powerOfTwo = 1;
+                int weight = 0;
+                for (int i = 0; i < weights.length; i++) {
+                    if ((powerOfTwo & state) != 0) {
+                        weight += weights[i];
+                    }
+                    powerOfTwo <<= 1;
+                }
+                return weight;
+            }
+
+            private static int statePrice(long state, int[] prices) {
+                long powerOfTwo = 1;
+                int price = 0;
+                for (int i = 0; i < prices.length; i++) {
+                    if ((powerOfTwo & state) != 0) {
+                        price += prices[i];
+                    }
+                    powerOfTwo <<= 1;
+                }
+                return price;
     }
 }
---
+++ src/homeWork33/Backpack/BackpackNew.java
@@ -0,0 +1,56 @@
+package homeWork33.Backpack;
+
+public class BackpackNew {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        int[] weights = {3, 4, 5, 8, 9, 5, 7, 9, 12, 15, 17, 18, 20};
+        int[] prices = {1, 6, 4, 7, 6, 4, 6, 10, 9, 10, 9, 12, 12};
+
+        int maxWeight = 56;
+
+        long count = 1 << weights.length;
+
+        int maxPrice = 0;
+        int maxState = 0;
+        int maxStateWeight = 0;
+        int itemCount = 0;
+        int totalPrice = 0;
+
+        for (int state = 0; state < count; state++) {
+            int price = 0;
+            int weight = 0;
+            int countItems = 0;
+            int total = 0;
+            for (int i = 0; i < weights.length; i++) {
+                if (((state >> i) & 1) == 1) {
+                    countItems++;
+                    total += prices[i];
+                    weight += weights[i];
+                }
+            }
+            if (weight <= maxWeight && total > maxPrice) {
+                maxPrice = total;
+                maxState = state;
+                maxStateWeight = weight;
+                itemCount = countItems;
+                totalPrice = total;
+            }
+        }
+
+        System.out.println("Оптимальное содержимое рюкзака:");
+        for (int i = 0; i < weights.length; i++) {
+            if (((maxState >> i) & 1) == 1) {
+                System.out.println("Элемент " + (i + 1) + " - стоимость: " + prices[i] + ", вес: " + weights[i]);
+            }
+        }
+
+        System.out.println("Число элементов в рюкзаке: " + itemCount);
+        System.out.println("Общая стоимость элементов в рюкзаке: " + totalPrice);
+        System.out.println("Общая стоимость рюкзака: " + maxPrice);
+        System.out.println("Общий вес рюкзака: " + maxStateWeight);
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Время выполнения: " + duration + " миллисекунд.");
+    }
+}
