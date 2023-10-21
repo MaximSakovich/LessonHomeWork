948eddd00460c80fb9dea4253a85e3fd1f52297d
Maksym Sakovych <maximsakovich@gmail.com>
2023-10-22 00:45:42 +0200

    TV

--- src/homeWork33/Task3Min2/Main.java
+++ src/homeWork33/Task3Min2/Main.java
@@ -24,26 +24,3 @@ public class Main {
                 System.out.println("Минимальный элемент в массиве: " + min);
             }
         }
-
-        /*   int [] array = new int[] {2, 42, 73, 41, 32, 53, 16, 24, 57, 42, 16, 55, 36};
-
-int min = Integer.MAX_VALUE;
-int min2 = Integer.MAX_VALUE;
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < min) {
-                min2 = min;
-                min = array[i];
-            } else if (array[i] < min2 && array[i] != min){
-                min2 = array[i];
-            }
-        }
-if (min2 != Integer.MAX_VALUE) {
-    System.out.println("Второй по минимальности элемент в массиве: " + min2);
-} else {
-    System.out.println("Второго по минимальности элемента нет");
-}
-        System.out.println("Минимальный элемент в массиве: " + min);
-    }
-}
-*/
