4d6ec6f036a0250e0717f94a78da0f13b4459553
Maksym Sakovych <maximsakovich@gmail.com>
2023-10-24 14:59:45 +0200

    Persons

---
+++ src/homeWork34/persons/PersonApp.java
@@ -0,0 +1,22 @@
+package homeWork34.persons;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Person person = new Person("email", "password");
+        System.out.println(person);
+        person.setEmail("email");
+
+        String test = "01@235689";
+        System.out.println(test.indexOf('@'));
+        System.out.println(test.lastIndexOf('@'));
+        System.out.println(test.indexOf('1'));
+        System.out.println(test.indexOf('1', 3));
+
+        System.out.println(person);
+        person.setEmail("a_sd@fff.ff");
+        System.out.println(person);
+
+        String password = "Password@1";
+        System.out.println("Действителен ли пароль? " + person.isPasswordValid(password));
+    }
+}
